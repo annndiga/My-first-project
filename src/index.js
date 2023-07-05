b05bdaeca71952146893bb0efb9879a6c9fbc447
Ann Ndiga <annndiga@gmail.com>
2023-07-05 21:28:54 +0300

    SIA_SIA-SHOP

--- src/index.js
+++ src/index.js
@@ -1,39 +1,127 @@
 
-// Fetch product data and display the products
+let cartItems = [];
+
+// Function to update the quantity of items in the cart
+function updateCartItemQuantity(title, newQuantity) {
+  const cartItem = cartItems.find((item) => item.title === title);
+  if (cartItem) {
+    cartItem.quantity = newQuantity;
+    renderCart();
+  }
+}
+
+// Function to render cart items
+function renderCart() {
+  const cartContainer = document.getElementById('cart');
+  cartContainer.innerHTML = ''; // Clear the previous cart items
+
+  cartItems.forEach((item) => {
+    const cartCard = document.createElement('div');
+    cartCard.classList.add('card', 'cart-card');
+
+    const title = document.createElement('h1');
+    title.classList.add('card-title');
+    title.textContent = item.title;
+    cartCard.appendChild(title);
+
+    const imageWrapper = document.createElement('div');
+    imageWrapper.classList.add('image-wrapper');
+    const image = document.createElement('img');
+    image.src = item.image;
+    image.alt = item.title;
+    image.classList.add('card-image');
+    imageWrapper.appendChild(image);
+    cartCard.appendChild(imageWrapper);
+
+    const description = document.createElement('p');
+    description.textContent = item.description;
+    cartCard.appendChild(description);
+
+    const category = document.createElement('p');
+    category.classList.add('category');
+    category.textContent = item.category;
+    cartCard.appendChild(category);
+
+    const rate = document.createElement('p');
+    rate.textContent = `Rate: ${item.rate}`;
+    cartCard.appendChild(rate);
+
+    const count = document.createElement('p');
+    count.textContent = `Count: ${item.count}`;
+    cartCard.appendChild(count);
+
+    const price = document.createElement('p');
+    price.textContent = `Price: $${item.price}`;
+    cartCard.appendChild(price);
+
+    const quantityWrapper = document.createElement('div');
+    quantityWrapper.classList.add('card-quantity');
+    const decreaseButton = document.createElement('button');
+    decreaseButton.classList.add('decrease-btn');
+    decreaseButton.textContent = '-';
+    decreaseButton.addEventListener('click', () => decreaseQuantity(item.title));
+    quantityWrapper.appendChild(decreaseButton);
+    const quantity = document.createElement('span');
+    quantity.classList.add('quantity');
+    quantity.textContent = item.quantity;
+    quantityWrapper.appendChild(quantity);
+    const increaseButton = document.createElement('button');
+    increaseButton.classList.add('increase-btn');
+    increaseButton.textContent = '+';
+    increaseButton.addEventListener('click', () => increaseQuantity(item.title));
+    quantityWrapper.appendChild(increaseButton);
+    cartCard.appendChild(quantityWrapper);
+
+    cartContainer.appendChild(cartCard);
+  });
+
+  updateCartItemsCount();
+}
+
+// Function to update the cart items count
+function updateCartItemsCount() {
+  const cartCount = document.getElementById('cart-count');
+  const totalItems = cartItems.reduce((count, item) => count + item.quantity, 0);
+  cartCount.textContent = totalItems;
+}
+
+// Function to increase quantity of cart item
+function increaseQuantity(title) {
+  const cartItem = cartItems.find((item) => item.title === title);
+  if (cartItem) {
+    cartItem.quantity++;
+    renderCart();
+  }
+}
+
+// Function to decrease quantity of cart item
+function decreaseQuantity(title) {
+  const cartItem = cartItems.find((item) => item.title === title);
+  if (cartItem && cartItem.quantity > 1) {
+    cartItem.quantity--;
+    renderCart();
+  }
+}
+
+// Fetch product data and populate cartItems array
 fetch('https://fakestoreapi.com/products')
   .then((response) => response.json())
-  .then((completedata) => {
-    let data = '';
-    completedata.forEach((values) => {
-      data += `
-      <div class="card">
-          <h1 class="title">${values.title}</h1>
-          <img src="${values.image}" alt="img" class="images">
-          <p class="category">${values.category}</p>
-          <p>${values.description}</p>
-          <p class="price-label">Price:</p>
-          <p class="price">${values.price}</p>
-          <p class="rate-label">Rate:</p>
-          <p class="rate">${values.rating && values.rating.rate ? values.rating.rate : 'Rating not available'}</p>
-          <p class="count-label">Count:</p>
-          <p class="count">${values.rating && values.rating.count ? values.rating.count : 'Rating count not available'}</p>
-          <button class="add-to-cart-btn" onclick="addToCart('${values.title}')">Add to Cart</button>
-      </div>`;
-    });
-    document.getElementById('cards').innerHTML = data;
+  .then((productData) => {
+    cartItems = productData.map((product) => ({
+      title: product.title,
+      price: product.price,
+      quantity: 1,
+      image: product.image,
+      description: product.description,
+      category: product.category,
+      rate: product.rating.rate,
+      count: product.rating.count
+    }));
 
-    // Fetch shopping cart data and update the cartItems array
-    fetch('https://fakestoreapi.com/carts')
-      .then((response) => response.json())
-      .then((cartData) => {
-        const cartProductIds = cartData.products.flatMap((product) => product.productId);
-        cartItems = completedata.filter((item) => cartProductIds.includes(item.id));
-        renderCart(); // Update the UI to display the cart items
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    renderCart(); // Render the initial cart items
   })
   .catch((err) => {
     console.log(err);
   });
+
+  
