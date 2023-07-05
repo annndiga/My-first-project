18738952363119981e2547d7163637fa740d67ef
Ann Ndiga <annndiga@gmail.com>
2023-07-05 13:55:58 +0300

    SIA_SIA-SHOP

--- src/index.js
+++ src/index.js
@@ -4,17 +4,22 @@ fetch('https://fakestoreapi.com/products').then((data)=>{
 })
 .then((completedata) =>{
    // console.log(completedata[2].title);
+  
    let data="";
    completedata.forEach((values)=> {
     data += `
     <div class="card">
         <h1 class="title">${values.title}</h1>
         <img src="${values.image}" alt="img" class="images">
+        <p class="category">${values.category}</p>
         <p>${values.description}</p>
-        <p class="category">${values.Category}</p>
-        <p class="price">${values.Price}</p>
-    </div>`;
-
+          <p class="rate-label">Rate:</p>
+          <p class="rate">${values.rate}</p>
+          <p class="count-label">count:</p>
+          <p class="count">${values.Rate}</p>
+          <p class="price-label">Price:</p>
+          <p class="price">${values.price}</p>
+      </div>`;
 });
 document.getElementById("cards").innerHTML=data;
 })
