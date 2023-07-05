d35fa1661c5d308eeaedcfb5edd9de3d15048999
Ann Ndiga <annndiga@gmail.com>
2023-07-05 18:10:00 +0300

    SIA_SIA-SHOP

--- src/index.js
+++ src/index.js
@@ -1,29 +1,39 @@
-fetch('https://fakestoreapi.com/products').then((data)=>{
-// console.log(data);
-            return data.json();
-})
-.then((completedata) =>{
-   // console.log(completedata[2].title);
-  
-   let data="";
-   completedata.forEach((values)=> {
-    data += `
-    <div class="card">
-        <h1 class="title">${values.title}</h1>
-        <img src="${values.image}" alt="img" class="images">
-        <p class="category">${values.category}</p>
-        <p>${values.description}</p>
-        <p class="price-label">Price:</p>
+
+// Fetch product data and display the products
+fetch('https://fakestoreapi.com/products')
+  .then((response) => response.json())
+  .then((completedata) => {
+    let data = '';
+    completedata.forEach((values) => {
+      data += `
+      <div class="card">
+          <h1 class="title">${values.title}</h1>
+          <img src="${values.image}" alt="img" class="images">
+          <p class="category">${values.category}</p>
+          <p>${values.description}</p>
+          <p class="price-label">Price:</p>
           <p class="price">${values.price}</p>
           <p class="rate-label">Rate:</p>
           <p class="rate">${values.rating && values.rating.rate ? values.rating.rate : 'Rating not available'}</p>
-          <p class="count-label">count:</p>
+          <p class="count-label">Count:</p>
           <p class="count">${values.rating && values.rating.count ? values.rating.count : 'Rating count not available'}</p>
-          
+          <button class="add-to-cart-btn" onclick="addToCart('${values.title}')">Add to Cart</button>
       </div>`;
-});
-document.getElementById("cards").innerHTML=data;
-})
-.catch((err) => {
+    });
+    document.getElementById('cards').innerHTML = data;
+
+    // Fetch shopping cart data and update the cartItems array
+    fetch('https://fakestoreapi.com/carts')
+      .then((response) => response.json())
+      .then((cartData) => {
+        const cartProductIds = cartData.products.flatMap((product) => product.productId);
+        cartItems = completedata.filter((item) => cartProductIds.includes(item.id));
+        renderCart(); // Update the UI to display the cart items
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
